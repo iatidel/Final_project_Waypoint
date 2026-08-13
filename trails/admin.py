4e673c6b74f0555363499e87a2a4a49c4321e0b3
iatidel akik <ch.iatidel@gmail.com>
2026-08-12 21:04:13 -0400

    WP-604: create superuser, register TrailAdmin with list_display and search_fields

--- trails/admin.py
+++ trails/admin.py
@@ -1,3 +1,32 @@
-from django.contrib import admin
+"""
+Application Programming CCGC 5003  Summer 2026
+Humber College Institute of Technology and Advanced Learning
+Waypoint - Web App
+Part 6 - The Database (WP-604)
+Developed by (IATIDEL AKIK N10038365)
 
-# Register your models here.
+Description:
+This module registers the Trail model with Django's admin panel,
+customizing how it appears - which columns show in the list view,
+and which fields are searchable.
+
+Classes:
+    TrailAdmin(admin.ModelAdmin) : customizes Trail's admin display
+"""
+
+from django.contrib import admin
+from .models import Trail
+
+
+class TrailAdmin(admin.ModelAdmin):
+    """
+    Customizes how Trail appears in the Django admin panel.
+    """
+    # Columns shown in the admin's trail list view
+    list_display = ("name", "distance_km", "elevation_gain", "difficulty", "is_open")
+
+    # Fields the admin search box will search through
+    search_fields = ("name",)
+
+
+admin.site.register(Trail, TrailAdmin)
